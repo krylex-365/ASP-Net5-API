d36599c466658006ff70e5b03b70626db2f0326e
AlE-dip <71812422+AlE-dip@users.noreply.github.com>
2021-04-07 09:13:48 +0700

    sua loi
    7/4/2021

--- ThuongMaiDienTu/ClientApp/src/app/login/login.component.ts
+++ ThuongMaiDienTu/ClientApp/src/app/login/login.component.ts
@@ -1,10 +1,6 @@
-import { Route } from '@angular/compiler/src/core';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { error } from 'console';
-import { environment } from '../../environments/environment.prod';
 import { Account } from '../../models/account';
-import { Login } from '../../models/login';
 import { LoginService } from '../../services/login.service';
 
 @Component({
@@ -32,14 +28,7 @@ export class LoginComponent {
     
     this.loginService.login(this.account)
       .subscribe(
-        (result) => {
-          if (result.status == 200) {
-            this.router.navigateByUrl(this.route.snapshot.queryParams.returnUrl || 'dashboard');
-          } else {
-            console.log(false);
-          }
-        },
-        error => console.log(false)
-      );
+        () => { this.router.navigateByUrl(this.route.snapshot.queryParams.returnUrl || 'dashboard') }
+      )
   }
 }
