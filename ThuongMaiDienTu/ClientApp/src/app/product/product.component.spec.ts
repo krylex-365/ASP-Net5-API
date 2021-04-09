308c199fdf383ed1aba83c3e647ff52f20824e21
Khánh Sora <67995752+Elahira@users.noreply.github.com>
2021-04-10 01:58:10 +0700

    product

---
+++ ThuongMaiDienTu/ClientApp/src/app/product/product.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { ProductComponent } from './product.component';
+
+let component: ProductComponent;
+let fixture: ComponentFixture<ProductComponent>;
+
+describe('product component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ ProductComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(ProductComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
---
+++ ThuongMaiDienTu/ClientApp/src/services/product.service.ts
@@ -0,0 +1,17 @@
+﻿import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+import { Product } from '../models/prodcut';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  url = environment.url;
+  constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get<Product[]>(this.url + 'api/Product');
+  }
+}
